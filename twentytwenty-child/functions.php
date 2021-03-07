ee105946b21af29f4fb2a52af5f20ae81273c592
Gaurav Kadnurrao <gkadnurrao1711@gmail.com>
2021-03-07 23:17:55 +0530

    enqueued styles and scripts

--- twentytwenty-child/functions.php
+++ twentytwenty-child/functions.php
@@ -1,17 +1,19 @@
 <?php
-include('./API/restapi.php');
-
+include('./API/acftorest.php');
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
 function child_scripts()
 {
     wp_enqueue_style( 'bootstrap-min.css', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '4.0.0' );
+    wp_enqueue_style( 'swiper-min.css', '//unpkg.com/swiper/swiper-bundle.min.css', array(), '4.0.0' );
     wp_enqueue_script( 'jquery', '//code.jquery.com/jquery-3.2.1.slim.min.js', array(), '3.2.1' );
     wp_enqueue_script( 'popper-min', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array(), '1.12.9' );
     wp_enqueue_script( 'bootstrap-min.js', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array(), '4.0.0' );
+    wp_enqueue_script( 'swiper-min.js', '//unpkg.com/swiper/swiper-bundle.min.js', array(), '4.0.0' );
 }
 
 add_action( 'wp_enqueue_scripts', 'child_scripts' );
