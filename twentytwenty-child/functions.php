5a4ab61ee01fefb57d981c78d77c09f332e55ef3
Gaurav Kadnurrao <gkadnurrao1711@gmail.com>
2021-03-07 11:54:58 +0530

    using the get_pages function created a parent function for getting all the data from the acf fields

--- twentytwenty-child/functions.php
+++ twentytwenty-child/functions.php
@@ -10,4 +10,17 @@ function enqueue_parent_styles() {
 function get_menu() {
     return wp_get_nav_menu_items('menu');
 }
+
+function get_all_data($request){
+    $posts = get_pages([
+        'post_status' => 'publish'
+    ]);
+    $controller = new WP_REST_Posts_Controller('post');
+    $array = [];
+    foreach ( $posts as $post ) {
+        $data = $controller->prepare_item_for_response($post,$request);
+        $array[] = $controller->prepare_response_for_collection($data);
+    } 
+    return $array;
+}
 ?>
