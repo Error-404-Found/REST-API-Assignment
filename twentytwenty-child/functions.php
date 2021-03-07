4af4887225da44766ff44141e7b1daea482672eb
Gaurav Kadnurrao <gkadnurrao1711@gmail.com>
2021-03-07 22:06:51 +0530

    enqueued style and script for frontend

--- twentytwenty-child/functions.php
+++ twentytwenty-child/functions.php
@@ -6,5 +6,12 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+function child_scripts()
+{
+    wp_enqueue_style( 'bootstrap-min.css', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '4.0.0' );
+    wp_enqueue_script( 'jquery', '//code.jquery.com/jquery-3.2.1.slim.min.js', array(), '3.2.1' );
+    wp_enqueue_script( 'popper-min', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array(), '1.12.9' );
+    wp_enqueue_script( 'bootstrap-min.js', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array(), '4.0.0' );
+}
 
-?>
+add_action( 'wp_enqueue_scripts', 'child_scripts' );
